c365b7f30da8902648250acea0db4f9df824ba6e
alexustmsk <alexustmsk@gmail.com>
2021-01-11 17:28:44 +0300

    9.1. String [#253303]

---
+++ src/ru/job4j/calculate/Out.java
@@ -0,0 +1,14 @@
+package ru.job4j.calculate;
+
+public class Out {
+    public static void main(String[] args) {
+        String lyrics = "Мама мыла раму";
+        System.out.println(lyrics);
+        String name = "Alex";
+        byte age =31;
+        String separate = " - ";
+        String surname = "Ustyuzhanin ";
+        String id = name + separate + surname + age + " years old";
+        System.out.println(id);
+    }
+}
