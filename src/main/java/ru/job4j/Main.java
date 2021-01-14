e6a5efa1348018a66565edadf1958fa130b2c97a
alexustmsk <alexustmsk@gmail.com>
2021-01-14 14:12:33 +0300

    2. Добавление Maven в существующий проект. [#449285]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -1,7 +1,22 @@
 package ru.job4j;
 
 public class Main {
+
+    private static double percent = 0.1;
+    private static int prize = 50;
+    private static int pay = 2;
+    private static double payD = pay;
+    private static double prizeD = prize;
+    private static double summPrize = percent * prizeD;
+    private static double cash = summPrize - payD;
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+        System.out.println(summPrize);
+        if (summPrize > pay){
+            //return cash;
+            System.out.println(cash);
+        }
+        System.out.println("0");
     }
 }
