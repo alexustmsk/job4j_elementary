96fb477fbfa35ad1960f310791d05a6bbb569492
alexustmsk <alexustmsk@gmail.com>
2021-01-17 22:49:50 +0300

    1.1.5. Циклы [6. Произведение чисел в диапазоне]

---
+++ src/main/java/ru/job4j/loop/MultiplicationLoop.java
@@ -0,0 +1,11 @@
+package ru.job4j.loop;
+
+public class MultiplicationLoop {
+    public static int mult(int a, int b) {
+        int result = 1;
+        for (;  a <= b; a++) {
+            result = result * a;
+        }
+        return result;
+    }
+}
