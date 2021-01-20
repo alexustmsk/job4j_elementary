147a9b28563a84e07b29b04ac86782d144a5bfb8
alexustmsk <alexustmsk@gmail.com>
2021-01-20 23:11:12 +0300

    1.1.5. Циклы [9.2. Найти сумму каждого второго числа в диапазоне]

---
+++ src/test/java/ru/job4j/loop/SecondSumTest.java
@@ -0,0 +1,19 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SecondSumTest {
+
+    @Test
+    public void test() {
+        assertEquals(1, SecondSum.sum(1, 2));
+        assertEquals(4, SecondSum.sum(1, 3));
+        assertEquals(4, SecondSum.sum(1, 4));
+        assertEquals(9, SecondSum.sum(1, 5));
+        assertEquals(6, SecondSum.sum(2, 5));
+        assertEquals(12, SecondSum.sum(0, 6));
+    }
+
+}
